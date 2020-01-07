1fcfa29491aeae213e704e986114b3164bbdfe6d
Latch Jack <latchlernj@gmail.com>
2020-01-07 11:13:14 +0000

    added game over function

--- starter-code/scripts/app.js
+++ starter-code/scripts/app.js
@@ -1,12 +1,14 @@
 function init() {
+
   //  dom variables
   const grid = document.querySelector('.grid')
   const squares = []
   let direction = null
   const pointCounter = document.querySelector('.points')
+  const messageBox = document.querySelector('.message')
 
   let points = 0
-  
+
   let speed = 400
   // eslint-disable-next-line no-unused-vars
   const timerId = setInterval(movePlayer, speed)
@@ -14,9 +16,8 @@ function init() {
   // game variables
   const width = 15
   // eslint-disable-next-line prefer-const
-  let playerLocation = [3, 2, 1]
-  // const foodLocation = Math.floor(Math.random() * squares.length)
-  
+  let snake = [3, 2, 1]
+
   Array(width * width).join('.').split('.').forEach(() => {
     // create 
     const square = document.createElement('div')
@@ -26,32 +27,32 @@ function init() {
   })
 
   function addPlayer() {
-    playerLocation.map(index => squares[index].classList.add('player'))
+    snake.map(index => squares[index].classList.add('player'))
   }
   addPlayer()
 
   function removePlayer() {
-    playerLocation.map(index => squares[index].classList.remove('player'))
+    snake.map(index => squares[index].classList.remove('player'))
   }
-  
-  // console.log(playerLocation[0])
-  // console.log(squares)
 
   function newFood() {
     const foodLocation = Math.floor(Math.random() * squares.length)
     squares[foodLocation].classList.add('food')
+    // if (squares[foodLocation].classList.contains('player')) {
+    //   console.log('oops!')
+    //   foodLocation += Math.floor(Math.random() * squares.length)
+    // }
   }
   newFood()
-  // console.log(squares)
-  // console.log(squares[playerLocation])
+
   function speedUp() {
-    speed = speed - 50
+    speed -= 50
   }
 
   function snakeEats() {
-    if (squares[playerLocation[0]].classList.contains('food')) {
-      squares[playerLocation[0]].classList.remove('food')
-      playerLocation.push('4')
+    if (squares[snake[0]].classList.contains('food')) {
+      squares[snake[0]].classList.remove('food')
+      snake.push('4', '4', '4', '4', '4', '4', '4', '4') // ! edit this for test - grow faster
       newFood()
       console.log(speed)
       speedUp()
@@ -63,103 +64,105 @@ function init() {
   }
   snakeEats()
 
-  // function autoMove() {
-  //   playerLocation++
-  //   // if (playerLocation % 15 === 1) { 
-  //   //   playerLocation = playerLocation - 14 
-  //   // }
-  // }
-
   function movePlayer() {
-    // console.log(squares[playerLocation[0]])
+    // console.log(squares[snake[0]])
     if (direction === 'right') {
       removePlayer()
-      playerLocation.pop()
-      if (playerLocation[0] % width === 14) {
-        playerLocation[0] -= 14
+      snake.pop()
+      if (snake[0] % width === 14) {
+        snake[0] -= 14
       }
-      playerLocation.unshift(playerLocation[0] + 1)
+      snake.unshift(snake[0] + 1)
       addPlayer()
-      // console.log(playerLocation)
+      // console.log(snake)
 
     }
     if (direction === 'left') {
       removePlayer()
-      playerLocation.pop()
-      if (playerLocation[0] % width === 0) {
-        playerLocation[0] += 14
+      snake.pop()
+      if (snake[0] % width === 0) {
+        snake[0] += 14
       }
-      playerLocation.unshift(playerLocation[0] - 1)
+      snake.unshift(snake[0] - 1)
       addPlayer()
-      // console.log(playerLocation)
+      // console.log(snake)
     }
     if (direction === 'down') {
       removePlayer()
-      playerLocation.pop()
-      if (playerLocation[0] >= 210) {
-        playerLocation[0] -= 210
+      snake.pop()
+      if (snake[0] >= 210) {
+        snake[0] -= 210
       }
-      playerLocation.unshift(playerLocation[0] + 15)
+      snake.unshift(snake[0] + 15)
       addPlayer()
-      // console.log(playerLocation)
+      // console.log(snake)
     }
     if (direction === 'up') {
       removePlayer()
-      playerLocation.pop()
-      if (playerLocation[0] <= 14) {
-        playerLocation[0] += 210
+      snake.pop()
+      if (snake[0] <= 14) {
+        snake[0] += 210
       }
-      playerLocation.unshift(playerLocation[0] - 15)
+      snake.unshift(snake[0] - 15)
       addPlayer()
-      // console.log(playerLocation)
+      // console.log(snake)
     }
     snakeEats()
+    collisionCheck()
   }
-  
-  
+
+  function collisionCheck() {
+    // if square is already occupied
+    if (snake.slice(1).includes(snake[0])) {
+      // game over
+      console.log('hit')
+      gameOver()
+      messageBox.innerHTML = 'GAME OVER!'
+    }
+  }
+
+  function gameOver() {
+    direction = undefined
+    removePlayer()
+  }
 
   function handleKeyDown(e) {
     switch (e.keyCode) {
       case 39: if (direction !== 'left') direction = 'right'
         movePlayer()
         console.log(direction)
-        // if (playerLocation % width < width - 1) {
-        //   console.log(squares[playerLocation])
-        //   playerLocation++
+        // if (snake % width < width - 1) {
+        //   console.log(squares[snake])
+        //   snake++
         // }
         break
       case 37: if (direction !== 'right') direction = 'left'
         movePlayer()
         console.log(direction)
-        // if (playerLocation % width > 0) {
-        //   playerLocation--
+        // if (snake % width > 0) {
+        //   snake--
         // }
         break
       case 40: if (direction !== 'up') direction = 'down'
         movePlayer()
         console.log(direction)
-        // if (playerLocation + width < width * width) {
-        //   playerLocation += width 
+        // if (snake + width < width * width) {
+        //   snake += width 
         // }
         break
       case 38: if (direction !== 'down') direction = 'up'
         movePlayer()
         console.log(direction)
-        // if (playerLocation - width >= 0) {
-        //   playerLocation -= width
+        // if (snake - width >= 0) {
+        //   snake -= width
         // }
         break
       default:
         console.log('player shouldnt move')
     }
 
-    // console.log('current player index is' , playerLocation)
+    // console.log('current player index is' , snake)
   }
-  
-  // if (squares[playerLocation[0]].classList.contains('food')) {
-  //   // console.log('food')
-  //   squares[playerLocation[0]].classList.remove('food')
-  // }
 
   window.addEventListener('keydown', handleKeyDown)
 }
